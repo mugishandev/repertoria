4be3c82f48877713b65fbcc64521bb1924b4ce2b
Maxime Jacquier <maxime74500@gmail.com>
2026-08-25 18:02:01 +0200

    LLM start ready

--- app/models/data_analyzer.rb
+++ app/models/data_analyzer.rb
@@ -1,4 +1,13 @@
+SYSTEM_PROMPT = ""
+
 class DataAnalyzer < ApplicationRecord
   belongs_to :user
   belongs_to :opening
+
+  def analyse
+    @games = Game.fetch_from_chess_com("mjnk")
+
+    ruby_llm_chat = RubyLLM.chat
+    response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@games.content)
+  end
 end
