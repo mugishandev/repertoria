35d50dea62c444aa44d26af514dea25f76af2e1c
Maxime Jacquier <maxime74500@gmail.com>
2026-08-25 17:09:08 +0200

    Call API

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,25 @@
 require "open-uri"
 require "json"
+require "net/http"
 
 class Game < ApplicationRecord
   belongs_to :user
 
-  # url = "https://api.chess.com/pub/player/#{@username}/games"
+  def self.fetch_from_chess_com(username)
+    archives_url = URI("https://api.chess.com/pub/player/#{username}/games/archives")
 
-  # response = URI.open(url).read
-  # game_data = JSON.parse(response)
+    response = Net::HTTP.get(archives_url)
+    data = JSON.parse(response)
 
+    last_archive = data["archives"].last
+
+    return [] if last_archive.nil?
+
+    games_url = URI(last_archive)
+
+    response = Net::HTTP.get(games_url)
+    data = JSON.parse(response)
+
+    data["games"]
+  end
 end
