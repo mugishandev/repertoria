31a54a25c8f9ecd02b8890b25b70ebfc8b1b5ead
Maxime Jacquier <maxime74500@gmail.com>
2026-08-26 10:54:52 +0200

    llm fonctionnel

--- app/models/data_analyzer.rb
+++ app/models/data_analyzer.rb
@@ -6,7 +6,6 @@ class DataAnalyzer < ApplicationRecord
 
   def analyse
     @games = Game.fetch_from_chess_com("mjnk")
-
     ruby_llm_chat = RubyLLM.chat
     response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@games.content)
   end
