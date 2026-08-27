341061b49a5dd683b3a2bcb45c73853542960195
arnaud muscatelli <arnaud.muscatelli+lewagon@gmail.com>
2026-08-27 16:43:57 +0200

    Onglets actifs

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -1,8 +1,33 @@
 class OpeningsController < ApplicationController
+  before_action :set_opening, only: [:show, :description, :explanation, :resources, :tab_content]
   def index
     @last_update = DataAnalyzer.maximum(:updated_at)
   end
 
   def show
+    @opening = Opening.find(params[:id])
   end
+
+  def description
+    render partial: "description"
+  end
+
+  def explanation
+    render partial: "explanation"
+  end
+
+  def resources
+    render partial: "resources"
+  end
+
+  def tab_content
+    render partial: "tab_content"
+  end
+
+  private
+
+  def set_opening
+    @opening = Opening.find(params[:id])
+  end
+
 end
