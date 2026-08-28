8b1a95927ed3ae26fb97f1e9e324cc8af68bf05d
Maxime Jacquier <maxime74500@gmail.com>
2026-08-28 13:02:42 +0200

    add videos and images to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@
 #   end
 
 openings = [
-  { name: "L'Italienne", suite_de_coups: "1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5 4.d3 Nf6 5.O-O O-O 6.Re1", color: "white", against: nil },
-  { name: "L'Écossaise", suite_de_coups: "1.e4 e5 2.Nf3 Nc6 3.d4 exd4 4.Nxd4 Nf6 5.Nxc6 bxc6 6.Bd3", color: "white", against: nil },
-  { name: "Le Système de Londres", suite_de_coups: "1.d4 d5 2.Nf3 Nf6 3.Bf4 e6 4.e3 Bd6 5.Bg3 O-O 6.Bd3", color: "white", against: nil },
-  { name: "La Scandinave", suite_de_coups: "1.e4 d5 2.exd5 Qxd5 3.Nc3 Qd8 4.d4 Nf6 5.Nf3 c6 6.Bc4", color: "black", against: "e4" },
-  { name: "La Française", suite_de_coups: "1.e4 e6 2.d4 d5 3.Nc3 Nf6 4.e5 Nfd7 5.f4 c5 6.Nf3", color: "black", against: "e4" },
-  { name: "La Caro-Kann", suite_de_coups: "1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Bf5 5.Ng3 Bg6 6.Nf3", color: "black", against: "e4" },
-  { name: "Le Gambit Dame refusé", suite_de_coups: "1.d4 d5 2.c4 e6 3.Nc3 Nf6 4.Nf3 Be7 5.Bg5 O-O 6.e3", color: "black", against: "d4" },
-  { name: "La Défense Slave", suite_de_coups: "1.d4 d5 2.c4 c6 3.Nf3 Nf6 4.Nc3 dxc4 5.a4 Bf5 6.e3", color: "black", against: "d4" },
-  { name: "La Défense Hollandaise", suite_de_coups: "1.d4 f5 2.g3 Nf6 3.Bg2 g6 4.Nf3 Bg7 5.O-O O-O 6.c4", color: "black", against: "d4" }
+  { name: "L'Italienne", suite_de_coups: "1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5 4.d3 Nf6 5.O-O O-O 6.Re1", color: "white", against: nil, video_url: "https://www.youtube.com/watch?v=5Ny8I1Bj-ek", image: "board_italienne.png" },
+  { name: "L'Écossaise", suite_de_coups: "1.e4 e5 2.Nf3 Nc6 3.d4 exd4 4.Nxd4 Nf6 5.Nxc6 bxc6 6.Bd3", color: "white", against: nil, video_url: "https://www.youtube.com/watch?v=nWWe_byE3gM", image: "board_ecossaise.png" },
+  { name: "Le Système de Londres", suite_de_coups: "1.d4 d5 2.Nf3 Nf6 3.Bf4 e6 4.e3 Bd6 5.Bg3 O-O 6.Bd3", color: "white", against: nil, video_url: "https://www.youtube.com/watch?v=k86o1Wql56c", image: "board_systeme_londres" },
+  { name: "La Scandinave", suite_de_coups: "1.e4 d5 2.exd5 Qxd5 3.Nc3 Qd8 4.d4 Nf6 5.Nf3 c6 6.Bc4", color: "black", against: "e4", video_url: "https://www.youtube.com/watch?v=1wHJGyWGVLA", image: "board_scandinave" },
+  { name: "La Française", suite_de_coups: "1.e4 e6 2.d4 d5 3.Nc3 Nf6 4.e5 Nfd7 5.f4 c5 6.Nf3", color: "black", against: "e4", video_url: "https://www.youtube.com/watch?v=vVz6xqqtP1U", image: "board_francaise" },
+  { name: "La Caro-Kann", suite_de_coups: "1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Bf5 5.Ng3 Bg6 6.Nf3", color: "black", against: "e4", video_url: "https://www.youtube.com/watch?v=hlL9WW5tAUw", image: "board_caro_kann" },
+  { name: "Le Gambit Dame refusé", suite_de_coups: "1.d4 d5 2.c4 e6 3.Nc3 Nf6 4.Nf3 Be7 5.Bg5 O-O 6.e3", color: "black", against: "d4", video_url: "https://www.youtube.com/watch?v=pE2cSSJdYRY", image: "board_gambit_dame_refuse" },
+  { name: "La Défense Slave", suite_de_coups: "1.d4 d5 2.c4 c6 3.Nf3 Nf6 4.Nc3 dxc4 5.a4 Bf5 6.e3", color: "black", against: "d4", video_url: "https://www.youtube.com/watch?v=_oHHm7MZAO8", image: "board_slave" },
+  { name: "La Défense Hollandaise", suite_de_coups: "1.d4 f5 2.g3 Nf6 3.Bg2 g6 4.Nf3 Bg7 5.O-O O-O 6.c4", color: "black", against: "d4", video_url: "https://www.youtube.com/watch?v=nD6bXC6RQkM", image: "board_hollandaise" }
 ]
 
 openings.each do |opening|
