8eb1145c623567aae9262d1e314c468c75fb2bc8
arnaud muscatelli <arnaud.muscatelli+lewagon@gmail.com>
2026-09-01 12:54:12 +0200

    Modif dashboard + Explanation

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts "seeding openings..."
 openings = [
   { name: "L'Italienne", suite_de_coups: "1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5 4.d3 Nf6 5.O-O O-O 6.Re1", color: "white", against: nil, video_url: "https://www.youtube.com/watch?v=5Ny8I1Bj-ek", image: "board_italienne.png" },
   { name: "L'Écossaise", suite_de_coups: "1.e4 e5 2.Nf3 Nc6 3.d4 exd4 4.Nxd4 Nf6 5.Nxc6 bxc6 6.Bd3", color: "white", against: nil, video_url: "https://www.youtube.com/watch?v=nWWe_byE3gM", image: "board_ecossaise.png" },
-  { name: "Le Système de Londres", suite_de_coups: "1.d4 d5 2.Nf3 Nf6 3.Bf4 e6 4.e3 Bd6 5.Bg3 O-O 6.Bd3", color: "white", against: nil, video_url: "https://www.youtube.com/watch?v=k86o1Wql56c", image: "board_systeme_londres" },
+  { name: "Le Système de Londres", suite_de_coups: "1.d4 d5 2.Nf3 Nf6 3.Bf4 e6 4.e3 Bd6 5.Bg3 O-O 6.Bd3", color: "white", against: nil, video_url: ["https://www.youtube.com/watch?v=k86o1Wql56c",], image: "board_systeme_londres" },
   { name: "La Scandinave", suite_de_coups: "1.e4 d5 2.exd5 Qxd5 3.Nc3 Qd8 4.d4 Nf6 5.Nf3 c6 6.Bc4", color: "black", against: "e4", video_url: "https://www.youtube.com/watch?v=1wHJGyWGVLA", image: "board_scandinave" },
   { name: "La Française", suite_de_coups: "1.e4 e6 2.d4 d5 3.Nc3 Nf6 4.e5 Nfd7 5.f4 c5 6.Nf3", color: "black", against: "e4", video_url: "https://www.youtube.com/watch?v=vVz6xqqtP1U", image: "board_francaise" },
   { name: "La Caro-Kann", suite_de_coups: "1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Bf5 5.Ng3 Bg6 6.Nf3", color: "black", against: "e4", video_url: "https://www.youtube.com/watch?v=hlL9WW5tAUw", image: "board_caro_kann" },
